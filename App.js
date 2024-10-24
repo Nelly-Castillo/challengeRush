93f79ab5794e7bd9e8d401f3ab46f4a4bfc53c41
NELLY CASTILLO CADENAS <99703789+Nelly-Castillo@users.noreply.github.com>
2024-10-24 14:15:06 -0600

    Add: screen files and cards

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {cards} from './cards.json'
+
+const card = cards["1"];
 
 export default function App() {
+  console.log(card);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
